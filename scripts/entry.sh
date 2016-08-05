f5619249fc80d234000917d4f0ead743b9b78f37
UVADave <dpg3k@virginia.edu>
2016-08-05 10:40:55 -0400

    Add env controlled debug support

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -6,6 +6,7 @@ DBPASSWD_OPT=""
 IMPORT_OPT=""
 EXPORT_OPT=""
 TOKENURL_OPT=""
+DEBUG_OPT=""
 
 # database host
 if [ -n "$DBHOST" ]; then
@@ -42,7 +43,12 @@ if [ -n "$TOKENAUTH_URL" ]; then
    TOKENURL_OPT="--tokenauth $TOKENAUTH_URL"
 fi
 
-bin/deposit-auth-ws $DBHOST_OPT $DBNAME_OPT $DBUSER_OPT $DBPASSWD_OPT $IMPORT_OPT $EXPORT_OPT $TOKENURL_OPT
+# service debugging
+if [ -n "$DEPOSITAUTH_DEBUG" ]; then
+   DEBUG_OPT="--debug"
+fi
+
+bin/deposit-auth-ws $DBHOST_OPT $DBNAME_OPT $DBUSER_OPT $DBPASSWD_OPT $IMPORT_OPT $EXPORT_OPT $TOKENURL_OPT $DEBUG_OPT
 
 #
 # end of file
