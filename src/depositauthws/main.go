f82be188578c813fb25731713595170d88c90b62
UVADave <dpg3k@virginia.edu>
2017-12-05 12:36:41 -0500

    Disable strict mode as the mysql driver no longer supports it

--- src/depositauthws/main.go
+++ src/depositauthws/main.go
@@ -17,7 +17,7 @@ func main() {
 	logger.Log(fmt.Sprintf("===> version: '%s' <===", handlers.Version()))
 
 	// access the database
-	connectStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?allowOldPasswords=1&strict=true&sql_notes=false", config.Configuration.DbUser,
+	connectStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?allowOldPasswords=1&sql_notes=false", config.Configuration.DbUser,
 		config.Configuration.DbPassphrase, config.Configuration.DbHost, config.Configuration.DbName)
 
 	err := dao.NewDB(connectStr)
