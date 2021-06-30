9b4d3fdb6a13b9a36bd8c54b0e53925c353b1e3c
Nestor Walker <naw4t@virginia.edu>
2021-06-30 12:05:38 -0400

    LIBRA-1199 Add MECULTE-MA, change Curry name

---
+++ sql/003_migration_mapping changes.sql
@@ -0,0 +1,8 @@
+--
+-- migration
+
+INSERT INTO fieldmapper( field_class, field_name, field_value ) VALUES( "department", "MECULTE-MA", "Media, Culture, and Technology - Graduate School of Arts and Sciences" );
+
+--
+-- end of file
+--
